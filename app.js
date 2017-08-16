5632f50e10f166c4d3fe4850ed30ce042381283f
neha.saha <neha.saha@workday.com>
2017-08-16 16:18:15 -0700

    simple express server

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var debug = require('debug');
+var log = debug('server:log');
+
+var port = process.env.PORT || 8080;
+
+app.get('/', function(req, res){
+    res.send('Hello World!!!');
+});
+
+app.listen(port);
+log('Express started on port ' + port);
